4de89f22f468ed3bf739dc0ba8cfe482862b7e1f
Moritz Laube <info@moritz-laube.de>
2022-05-30 12:03:25 +0200

    Add static folder and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.moritzlaube.com",
   publicDir: "./static",
   outDir: "./public",
   integrations: [svelte(), sitemap()],
